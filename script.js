9bdb174157a7630e57b9f210e1cb020fe6ccd83f
SamyakLearner <43209060+SamyakLearner@users.noreply.github.com>
2019-03-22 09:15:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+const btns = document.querySelectorAll(".btn");
+const screen = document.querySelector(".screen");
+const equalBtn = document.querySelector(".btn-equal");
+const clearBtn = document.querySelector(".btn-clear");
+
+btns.forEach( (btn) => {
+    btn.addEventListener("click",() => {
+        let number = btn.getAttribute("data-num");
+        screen.value += number;
+    });
+});
+
+equalBtn.addEventListener("click",() => {
+    if(screen.value === '')
+    {screen.value = 'Please enter a value';}
+    else
+    {let value = eval(screen.value);
+    screen.value = value;}
+});
+
+clearBtn.addEventListener("click", () => {
+    screen.value = "";
+});
